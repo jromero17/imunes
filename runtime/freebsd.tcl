1ff18487ad39c720bf6e56c8c2ff9605b2f90044
José Manuel Romero Herrera <panake2000@gmail.com>
2025-01-14 19:45:15 -0400

    Update freebsd.tcl active dhcp

--- runtime/freebsd.tcl
+++ runtime/freebsd.tcl
@@ -2015,6 +2015,11 @@ proc getExtIfcs { } {
 }
 
 proc getIPv4IfcCmd { ifc addr primary } {
+    # DESDE AQUI
+    if { $addr == "dhcp" } {
+	    return "dhclient -b $ifc"
+    }
+    # HASTA AQUI
     if { $primary } {
 	return "ifconfig $ifc inet $addr"
     }
