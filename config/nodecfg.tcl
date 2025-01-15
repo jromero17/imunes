bb81935645691707b53c16063b7cd6b3c71bda1d
José Manuel Romero Herrera <panake2000@gmail.com>
2025-01-14 20:18:18 -0400

    Update nodecfg.tcl

--- config/nodecfg.tcl
+++ config/nodecfg.tcl
@@ -1566,11 +1566,6 @@ proc getDefaultRoutesConfig { node gws } {
 
 	set match4 false
 	foreach ipv4_addr $ipv4_addrs {
-        # DESDE AQUI 
-	    if { $ipv4_addr == "dhcp" } {
-		    continue
-	    }
-        # HASTA AQUI
 	    set mask [ip::mask $ipv4_addr]
 	    if { [ip::prefix $gateway4/$mask] == [ip::prefix $ipv4_addr] } {
 		set match4 true
